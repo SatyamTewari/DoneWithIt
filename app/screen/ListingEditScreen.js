311c4907dfb22201e843ce068bd374b6817aabfb
SatyamTewari <satyamtewari13@gmail.com>
2022-03-06 18:38:30 +0530

    getUserLocation custom hook added and navigation libraries installed

--- app/screen/ListingEditScreen.js
+++ app/screen/ListingEditScreen.js
@@ -1,8 +1,9 @@
+import CustomPicker from '../components/CustomPicker';
+import { AppForm, AppFormField, FormImagePicker, SubmitButton } from '../components/forms';
 import React from 'react';
 import { View, StyleSheet, StatusBar } from 'react-native';
 import * as Yup from 'yup';
-import CustomPicker from '../components/CustomPicker';
-import { AppForm, AppFormField, FormImagePicker, SubmitButton } from '../components/forms';
+import useLocation from '../hooks/useLocation';
 
 const validationSchema = Yup.object().shape({
     title: Yup.string().required().min(1).label('Title'),
@@ -18,51 +19,53 @@ const categories = [
     {label: 'Camera', value: 3}
 ]
 function ListingEditScreen(props) {
-  return (
-    <View style={styles.container}>
-        <StatusBar/>
-        <AppForm
-            initialValues={{
-                title: '',
-                price: '',
-                description: '',
-                category: categories[0],
-                images: []
-            }}
-            onSubmit={(values) => console.log(values)}
-            validationSchema={validationSchema}
-        >
-            <FormImagePicker 
-                name='images'/>
-            <AppFormField 
-                placeholder='Title'
-                autoCapitalize='none'
-                keyboardType='email-address'
-                name='title'
-                maxLength={255}/>
-            <AppFormField 
-                placeholder='Price'
-                keyboardType='number-pad'
-                name='price'/>
-            <CustomPicker
-                name='category'
-                itemsList={categories}
-                prompt='Please choose a Category' />
-            <AppFormField 
-                placeholder='Description'
-                autoCapitalize='none'
-                autoCorrect={false}
-                keyboardType='email-address'
-                name='description'
-                numberOfLines={3}
-                multiline={true}
-                maxLength={255}/>
-            <View style={styles.buttonContainer}>
-                <SubmitButton title='Post'/>
-            </View>    
-        </AppForm>
-    </View>
-  );
+    const location = useLocation();
+
+    return (
+        <View style={styles.container}>
+            <StatusBar/>
+            <AppForm
+                initialValues={{
+                    title: '',
+                    price: '',
+                    description: '',
+                    category: categories[0],
+                    images: []
+                }}
+                onSubmit={(values) => console.log(values, location)}
+                validationSchema={validationSchema}
+            >
+                <FormImagePicker 
+                    name='images'/>
+                <AppFormField 
+                    placeholder='Title'
+                    autoCapitalize='none'
+                    keyboardType='email-address'
+                    name='title'
+                    maxLength={255}/>
+                <AppFormField 
+                    placeholder='Price'
+                    keyboardType='number-pad'
+                    name='price'/>
+                <CustomPicker
+                    name='category'
+                    itemsList={categories}
+                    prompt='Please choose a Category' />
+                <AppFormField 
+                    placeholder='Description'
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    keyboardType='email-address'
+                    name='description'
+                    numberOfLines={3}
+                    multiline={true}
+                    maxLength={255}/>
+                <View style={styles.buttonContainer}>
+                    <SubmitButton title='Post'/>
+                </View>    
+            </AppForm>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
